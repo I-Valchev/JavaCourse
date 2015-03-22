869457fbe26ec4e0ad0407b565d6ca382e6d8eaa
I-Valchev <Ivo.V.Valchev@gmail.com>
2015-03-22 12:38:53 +0200

    Submitting tasks for week 1 part 1

---
+++ week1/part1/week1.java
@@ -0,0 +1,35 @@
+package test;
+
+public class week1 {
+	
+	public int getNumberOfDigits(int num){
+		return Integer.toString(num).length();
+	}
+	
+	public String joinStrings(String glue, String ... args){
+		StringBuilder result = new StringBuilder();
+		
+		int i = 0;
+		for(String word : args){
+			result.append(word);
+			if(i != args.length-1) result.append(glue);
+			++i;
+		}
+		
+		return result.toString();
+	}
+	
+	public String reduceFilePath(String path){
+		String result = path;
+	
+		while(result.contains(("/./"))){ //replace recursively
+			result = result.replaceAll("/./", "/");
+		}
+		result = result.replaceAll("//", "/");
+		result = result.replaceAll("/([\\S]*)/../", "/");
+		result = result.replaceAll("/../", "/"); //single case for "/../etc..."
+		
+		if(result.endsWith("/") && result.length() != 1) result = result.substring(0, result.length()-1);
+		return result;
+	}
+}
---
+++ week1/part1/week1Test.java
@@ -0,0 +1,33 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class week1Test{
+
+	@Test
+	public void test() {
+		assertEquals(4, new week1().getNumberOfDigits(1444));
+	}
+	
+	@Test
+	public void joinStringsTest(){
+		assertEquals("I join strings", new week1().joinStrings(" ","I","join", "strings"));
+	}
+	
+	@Test
+	public void reduceFilePathTest(){
+		assertEquals("/", new week1().reduceFilePath("/"));
+		
+		assertEquals("/", new week1().reduceFilePath("/srv/../"));
+		assertEquals("/srv/www/htdocs/wtf", new week1().reduceFilePath("/srv/www/htdocs/wtf/"));
+		assertEquals("/srv/www/htdocs/wtf", new week1().reduceFilePath("/srv/www/htdocs/wtf"));
+		assertEquals("/srv", new week1().reduceFilePath("/srv/./././././"));
+		assertEquals("/etc/wtf", new week1().reduceFilePath("/etc//wtf/"));
+		assertEquals("/", new week1().reduceFilePath("/etc/../etc/../etc/../"));
+		assertEquals("/", new week1().reduceFilePath("//////////////"));
+		assertEquals("/", new week1().reduceFilePath("/../"));
+	}
+
+}
